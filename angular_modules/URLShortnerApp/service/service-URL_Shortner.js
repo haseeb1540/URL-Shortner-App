223fb1a8097c4cf853eb64d4bc1b00350c8d80df
haseebkappan <haseebkappanvga@gmail.com>
2020-06-22 21:25:20 +0530

    commit

--- angular_modules/URLShortnerApp/service/service-URL_Shortner.js
+++ angular_modules/URLShortnerApp/service/service-URL_Shortner.js
@@ -1,17 +1,17 @@
 
-angular.module('URLshortnerApp').service('serviceShortner', ['$http', function($http) {
+// angular.module('URLshortnerApp').service('serviceShortner', ['$http', function($http) {
 
-    this.fnGetData = function(data) {
-        var promise = $http({
-            url: 'http://127.0.0.1:3000/' + 'node/',
-            data: { data:data },
-            method: "POST",
-            withCredentials: false,
-            contentType: "application/json",
-            dataType: "json",
-        });
-        return promise;
-    };
+//     this.fnGetData = function(data) {
+//         var promise = $http({
+//             url: 'http://127.0.0.1:3000/' + 'node/',
+//             data: { data:data },
+//             method: "POST",
+//             withCredentials: false,
+//             contentType: "application/json",
+//             dataType: "json",
+//         });
+//         return promise;
+//     };
 
 
-}]);
+// }]);
